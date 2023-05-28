cef952781dd2faa3de7659d3eabaeac44699e2a2
0xTheDad <ekitaeff@yandex.ru>
2023-05-28 20:55:00 +0300

    Update README

--- src/feature_20251124.py
+++ src/feature_20251124.py
@@ -279,3 +279,8 @@ def historical_feature():
     """Feature added on 2023-10-26 18:49:00"""
     print('Historical feature working')
     return True
+# Historical update 2023-05-28 20:55:00
+def historical_feature():
+    """Feature added on 2023-05-28 20:55:00"""
+    print('Historical feature working')
+    return True
