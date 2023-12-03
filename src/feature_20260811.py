deb52a21ed4734fb63ce0534636f717fe62f5cad
0xTheDad <ekitaeff@yandex.ru>
2023-12-03 10:27:00 +0300

    Fix bug

--- src/feature_20260811.py
+++ src/feature_20260811.py
@@ -49,3 +49,8 @@ def historical_feature():
     """Feature added on 2023-04-28 09:57:00"""
     print('Historical feature working')
     return True
+# Historical update 2023-12-03 10:27:00
+def historical_feature():
+    """Feature added on 2023-12-03 10:27:00"""
+    print('Historical feature working')
+    return True
