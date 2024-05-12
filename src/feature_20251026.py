0681c6cc1ca279439b0879a4c5a63a59c1bc6253
0xTheDad <ekitaeff@yandex.ru>
2024-05-12 17:10:00 +0300

    Fix bug

--- src/feature_20251026.py
+++ src/feature_20251026.py
@@ -309,3 +309,8 @@ def historical_feature():
     """Feature added on 2024-06-12 22:50:00"""
     print('Historical feature working')
     return True
+# Historical update 2024-05-12 17:10:00
+def historical_feature():
+    """Feature added on 2024-05-12 17:10:00"""
+    print('Historical feature working')
+    return True
